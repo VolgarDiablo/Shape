b7b505b13d67b182c81141570b9e3b26783dabd2
Антон Дидковский <didkovskiy.anton@gmail.com>
2023-02-28 00:31:36 +0200

    add interface cloneable

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ShapePrinterName printer = new ShapePrinterName();
+//        ShapePrinterName printer = new ShapePrinterName();
 
         Shape square = new Square(5.3, "Blue");
         Shape rectangle = new Rectangle(23.4, 84.3, "Green");
@@ -11,22 +11,35 @@ public class Main {
         Shape triangle = new Triangle(10.6, 47.2, "Grey");
         Shape circle = new Circle(12.3, "Green");
 
-        List<Shape> shapeList = new ArrayList<>();
-        shapeList.add(square);
-        shapeList.add(rectangle);
-        shapeList.add(cube);
-        shapeList.add(triangle);
-        shapeList.add(circle);
-
-        for (Shape elem : shapeList) {
-            printer.printShapeName(elem);
+        try {
+            Shape clonedSquare = (Shape) square.clone();
+            System.out.println(square==clonedSquare);
+            System.out.println(square.equals(clonedSquare));
+            System.out.println(square.toString());
+            System.out.println(clonedSquare.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        for (Shape elem : shapeList) {
-            System.out.println(elem.getDescription());
-        }
-        for (Shape elem : shapeList) {
-            System.out.println(elem.getName());
-        }
+
+//        List<Shape> shapeList = new ArrayList<>();
+//        shapeList.add(square);
+//        shapeList.add(rectangle);
+//        shapeList.add(cube);
+//        shapeList.add(triangle);
+//        shapeList.add(circle);
+
+
+//        for (Shape elem : shapeList) {
+//            printer.printShapeName(elem);
+//        }
+
+//        for (Shape elem : shapeList) {
+//            System.out.println(elem.getDescription());
+//        }
+
+//        for (Shape elem : shapeList) {
+//            System.out.println(elem.getName());
+//        }
     }
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,12 @@
-public abstract class Shape {
+public abstract class Shape implements Cloneable{
     public abstract String getDescription();
+
     public abstract String getName();
+
     public abstract double getArea();
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
